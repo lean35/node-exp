a3447dea893482d2e6ee3931949a0bf3ec50edca
Henry Lee <dailychoices@lean35.com>
2017-05-18 15:13:08 -0400

    init

---
+++ app.js
@@ -0,0 +1,12 @@
+// lynda.com
+var express = require("express"); 
+
+var app = express() ; 
+
+var messages = [{email: 'dailychoices@gmail.com', owner:'Henry'}, {email: 'sycjang@gmail.com', owner:'Sungyon'}]
+app.get('/messages', function(req, res){
+    //res.send('Hello World11') ; 
+    res.json(messages); 
+}) ; 
+
+app.listen(1234) ; 
